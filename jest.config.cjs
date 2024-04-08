f69551278ced8a49e6d0411932c22414800414c7
Jason Norwood-Young <jason@10layer.com>
2024-04-08 11:04:01 +0200

    Jest config

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};

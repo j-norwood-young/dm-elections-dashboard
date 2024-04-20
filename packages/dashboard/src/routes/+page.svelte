78b62b98ee93dae9d7abc284a90e5432d194266f
Mina Demian <mina@minademian.com>
2024-04-20 20:15:08 +0200

    refactor: move to separate imports

--- packages/dashboard/src/routes/+page.svelte
+++ packages/dashboard/src/routes/+page.svelte
@@ -1,66 +1,31 @@
 <script>
 	import Hemicycle from 'svelte-hemicycle';
-
-	import data2014 from '$lib/data/seats_2014.json';
-	const Data2014 = data2014.partyResults;
-	import data2019 from '$lib/data/seats_2019.json';
-	const Data2019 = data2019.partyResults;
-	import colors from '$pkg/color-scheme.json';
-
-	let data, total_seats, heading, text_position;
-	data = Data2014;
-	total_seats = 400;
-	heading = '2014';
-
-	const calculateSeats = (arr) => arr.reduce((partialSum, item) => partialSum + item.count, 0);
-	const show = (year) => {
-		data = null;
-		switch (year) {
-			case '2014':
-				data = Data2014;
-				heading = '2014';
-				break;
-			case '2019':
-				data = Data2019;
-				heading = '2019';
-				break;
-			case '2024':
-				heading = '2024';
-				break;
-		}
-		total_seats = calculateSeats(data);
-		const modified = data.map((item) => ({
-			...item,
-			text: `${item.text} (${item.count})`
-		}));
-		data = modified;
-	};
-	const toggleColorScheme = (scheme) => {
-		let selectedScheme;
-		switch (scheme) {
-			case 'high':
-				selectedScheme = colors.colorSchemes[0].high;
-				break;
-			case 'low':
-				selectedScheme = colors.colorSchemes[1].low;
-				break;
-			case 'dm':
-				selectedScheme = colors.colorSchemes[2].dm;
-				break;
-		}
-
-		const filled = data.map((item, index) => ({
-			...item,
-			color: Object.entries(selectedScheme).at(index)[1]
-		}));
-		data = filled;
-	};
+	import { toggleColorScheme, decorateWithColors } from '$lib/load-colors';
+	import { generateHemicycleInformation, loadDefaultDataSet } from '$lib/load-data';
 
 	const rows = 12;
 	const display = ['points', 'text'];
 	const color = 'black';
 	const font_size = '20';
 	const selectedShape = 'hexagon';
+
+	let data, total_seats, heading, selectedScheme;
+
+	data = loadDefaultDataSet();
+	total_seats = 400;
+	heading = '2014';
+
+	const loadData = (year) => {
+		const { title, seats, modified } = generateHemicycleInformation(year);
+		heading = title;
+		total_seats = seats;
+		data = modified;
+	};
+
+	const manipulateHemicycle = (scheme) => {
+		selectedScheme = toggleColorScheme(scheme);
+		data = selectedScheme && decorateWithColors(data, selectedScheme);
+	};
 </script>
 
 <svelte:head>

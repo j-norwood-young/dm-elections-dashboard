1353135748126248fb6b9cebe5b7ec973975dd03
Mina Demian <mina@minademian.com>
2024-04-30 04:04:02 +0200

    chore: remove hardcoded values, center headings

--- packages/tabular-data/tabular-results.svelte
+++ packages/tabular-data/tabular-results.svelte
@@ -11,7 +11,7 @@
 
 <main>
   <Table color="custom" class="text-white">
-    <TableHead class="bg-gray-900">
+    <TableHead class="bg-gray-900 text-center">
       <TableHeadCell><span class="headingColor">Party</span></TableHeadCell>
       <TableHeadCell><span class="headingColor">% Votes</span></TableHeadCell>
       <TableHeadCell
@@ -19,31 +19,11 @@
       >
     </TableHead>
     <TableBody tableBodyClass="text-white font-thin text-xs">
-      <TableBodyRow class="bg-gray-800 border-b-0">
-        <TableBodyCell>Apple MacBook Pro 17"</TableBodyCell>
-        <TableBodyCell>Sliver</TableBodyCell>
-        <TableBodyCell>Laptop</TableBodyCell>
-      </TableBodyRow>
-      <TableBodyRow class="bg-gray-700 border-b-0">
-        <TableBodyCell>Microsoft Surface Pro</TableBodyCell>
-        <TableBodyCell>White</TableBodyCell>
-        <TableBodyCell>Laptop PC</TableBodyCell>
-      </TableBodyRow>
-      <TableBodyRow class="bg-gray-800 border-b-0">
-        <TableBodyCell>Magic Mouse 2</TableBodyCell>
-        <TableBodyCell>Black</TableBodyCell>
-        <TableBodyCell>Accessories</TableBodyCell>
-      </TableBodyRow>
-      <TableBodyRow class="bg-gray-700 border-b-0">
-        <TableBodyCell>Google Pixel Phone</TableBodyCell>
-        <TableBodyCell>Gray</TableBodyCell>
-        <TableBodyCell>Phone</TableBodyCell>
-      </TableBodyRow>
-      <TableBodyRow class="bg-gray-800 border-b-0">
-        <TableBodyCell>Apple Watch 5</TableBodyCell>
-        <TableBodyCell>Red</TableBodyCell>
-        <TableBodyCell>Wearables</TableBodyCell>
-      </TableBodyRow>
+      <TableBodyRow class="bg-gray-800 border-b-0"></TableBodyRow>
+      <TableBodyRow class="bg-gray-700 border-b-0"></TableBodyRow>
+      <TableBodyRow class="bg-gray-800 border-b-0"></TableBodyRow>
+      <TableBodyRow class="bg-gray-700 border-b-0"></TableBodyRow>
+      <TableBodyRow class="bg-gray-800 border-b-0"></TableBodyRow>
     </TableBody>
   </Table>
 </main>

a0fb720b04589ac2d2cc4a85cfbee5cc96a1d8f4
Mina Demian <mina@minademian.com>
2024-04-30 11:10:30 +0200

    chore: tooling

--- packages/dashboard/vite.config.ts
+++ packages/dashboard/vite.config.ts
@@ -2,10 +2,12 @@ import postCssConfig from "./postcss.config.mjs";
 import path from "path";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  preprocess: vitePreprocess(),
   css: postCssConfig,
   resolve: {
     alias: {

33ea39a5b735bfca24a40ad0feb923745e581e31
Jason Norwood-Young <jason@10layer.com>
2024-05-01 16:43:46 +0200

    Remove original Wordpress block code

--- dm-election-block/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c49865e23d35d60e5534');
--- dm-election-block/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'e85fee43bf805d74269a');

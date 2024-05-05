f054eaa08a13758ec999ec605770850f1883be09
Segundev <jayesegun03@gmail.com>
2024-05-05 18:24:23 +0100

    national and provincial view

--- packages/election-engine-map/src/App.svelte
+++ packages/election-engine-map/src/App.svelte
@@ -1,60 +1,96 @@
 <script>
+  import { votes2009Data, votes2014Data, votes2019Data, votes2024Data } from "./lib/libs/loadData.js";
   import { onMount } from "svelte";
-  import BackgroundMap from "./lib/backgroundMap.svelte";
+  import NationalView from "./lib/components/dashboard-view/nationalView.svelte";
+  import ProvincialView from "./lib/components/dashboard-view/provincialView.svelte";
 
   let data;
+  let viewBox = "national";
 
   onMount(async () => {
-    const response = await fetch("https://iec-api.revengine.dailymaverick.co.za/results/votes/national/2019");
+    // const response = await fetch("https://iec-api.revengine.dailymaverick.co.za/results/votes/national/2019");
 
-    data = await response.json();
+    // data = await response.json();
+
+    data = await votes2019Data();
   });
-
-  $: console.log(data);
 
   let innerWidth = 0;
 </script>
 
 <svelte:window bind:innerWidth />
 
-{#if data}
-  <div class="map-section">
-    <div class="select-wrapper">
-      <div class="select-button-wrapper">
-        <button> National Assembly</button>
-        <button> Provincial Legislature </button>
+<div class="map-section">
+  <div class="select-wrapper">
+    <div class="select-button-wrapper">
+      <div class="region">
+        <button class:national={viewBox === "national"} on:click={() => (viewBox = "national")}>
+          National Assembly</button
+        >
+        <button class:province={viewBox === "province"} on:click={() => (viewBox = "provincial")}>
+          Provincial Legislature
+        </button>
       </div>
-      <p>
-        200 Regional Seats, divided between the regions* based on registered population, prior to the elections. The
-        regions are contested by parties and independent candidates.
-      </p>
-      <div class="select-button-wrapper">
+    </div>
+    <div class="select-button-wrapper">
+      <div class="election-year">
+        <button> 2009</button>
         <button> 2014</button>
         <button> 2019 </button>
         <button> 2024</button>
       </div>
     </div>
-
-    <BackgroundMap {innerWidth} {data} />
   </div>
-{:else}
-  <p>..Loading</p>
-{/if}
+
+  {#if viewBox === "national"}
+    <NationalView {data} {innerWidth} />
+  {:else if viewBox === "provincial"}
+    <ProvincialView />
+  {/if}
+</div>
 
 <style>
   .map-section {
-    padding: 1.5rem 1rem;
+    padding: 1rem 1rem;
   }
   .select-wrapper {
     position: relative;
     z-index: 3;
   }
 
-  .select-wrapper p {
-    padding: 12px 0;
-  }
   .select-button-wrapper {
-    text-align: center;
     margin: 0 auto;
+  }
+
+  .select-button-wrapper .region,
+  .select-button-wrapper .election-year {
+    display: flex;
+    gap: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  .election-year button {
+    border: none;
+    border-bottom: 4px solid silver;
+    width: 75px;
+  }
+
+  .election-year button:hover {
+    outline: 4px auto black;
+    border-bottom: 4px solid black;
+    transition: border-color 0.3s;
+  }
+
+  .election-year button:focus,
+  .election-year button:focus-visible {
+    background-color: black;
+    color: white;
+    border-bottom: 4px solid black;
+  }
+
+  button.national,
+  button.province {
+    background-color: #0c0c0c;
+    color: #fff;
   }
 </style>
---
+++ packages/election-engine-map/src/lib/components/backgroundMap.svelte
@@ -0,0 +1,69 @@
+<script>
+  import cartogramResultShow from "./result-view/cartogramResultShow.svelte";
+
+  import geoData from "../../data/sa-province.json";
+  import { geoArea, geoIdentity, geoPath } from "d3-geo";
+  import CartogramResultShow from "./result-view/cartogramResultShow.svelte";
+  import CartogramNoResult from "./result-view/cartogramNoResult.svelte";
+
+  export let data;
+  export let innerWidth;
+
+  $: grid = innerWidth < 600 ? true : false;
+
+  let width = 600;
+  let height = 600;
+
+  let provinces = geoData.features;
+
+  /**
+   * geoIdentity - new geographic projection with an identity transformation
+   * [20, 20] - Minimum X and Y coordinates for the map
+   * [width, height] - set the map dimensions
+   * data - Data to fit the extent
+   */
+  const projection = geoIdentity()
+    .reflectY(true)
+    .fitExtent(
+      [
+        [20, 20],
+        [width, height],
+      ],
+      geoData,
+    );
+
+  // Geographic path generator based on the projection configured above.
+  const path = geoPath(projection);
+
+  //console.log(data);
+</script>
+
+<div class="svg-wrapper">
+  {#if !grid}
+    <svg class:grid={innerWidth < 400 ? false : true} {width} {height}>
+      <!-- Countries -->
+      <g id="saMap">
+        {#each provinces as province}
+          <path d={path(province)} fill="#ECECEC" stroke="white" stroke-width="0.94" />
+        {/each}
+      </g>
+    </svg>
+  {/if}
+  <CartogramNoResult {path} {grid} {provinces} />
+  <CartogramResultShow {provinces} {path} {grid} {data} />
+</div>
+
+<style>
+  .svg-wrapper {
+    position: relative;
+    height: 100vh;
+    margin-top: -5.75rem;
+  }
+
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>

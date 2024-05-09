7b9224e5fc5cd6380f9f7489095d0428ba8b88ba
Jason Norwood-Young <jason@10layer.com>
2024-05-09 16:44:16 +0200

    Documentation

--- packages/common/colors.js
+++ packages/common/colors.js
@@ -5,6 +5,14 @@ for (const [party, color] of Object.entries(partyColors)) {
     colors.push({ party, color });
 }
 
+/**
+ * Returns the color associated with a given party ID.
+ * If the party ID is not found in the colors array, a backup color is returned based on the index.
+ *
+ * @param {string} partyID - The ID of the party.
+ * @param {number} index - The index used to calculate the backup color.
+ * @returns {string} The color associated with the party ID or a backup color.
+ */
 export function partyColor(partyID, index) {
     const color = colors.find((d) => partyID === d.party);
     if (color) {

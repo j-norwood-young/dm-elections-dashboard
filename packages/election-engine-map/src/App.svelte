25a03e9f87949b5da945ba62a97588182b025a67
Jason Norwood-Young <jason@10layer.com>
2024-05-09 16:45:36 +0200

    - new loadData - Use centralised years

--- packages/election-engine-map/src/App.svelte
+++ packages/election-engine-map/src/App.svelte
@@ -1,10 +1,11 @@
 <script>
   // @ts-nocheck
 
-  import { votes2009Data, votes2014Data, votes2019Data, votes2024Data } from "./lib/libs/loadData.js";
+  import { loadData } from "./lib/libs/loadData.js";
   import { onMount } from "svelte";
   import NationalView from "./lib/components/dashboard-view/nationalView.svelte";
   import ProvincialView from "./lib/components/dashboard-view/provincialView.svelte";
+  import years from "@election-engine/common/years.json";
 
   // Parameters
   export let selected_year = 2019; // 2024, 2019, 2014
@@ -12,42 +13,21 @@
 	export let selected_region = 'National'; // National, Gauteng, Western Cape, etc.
 
   let data;
-
-  async function loadData() {
-    switch (selected_year) {
-      case 2009:
-        data = await votes2009Data();
-        break;
-      case 2014:
-        data = await votes2014Data();
-        break;
-      case 2019:
-        data = await votes2019Data();
-        break;
-      case 2024:
-        data = await votes2024Data();
-        break;
-      default:
-        data = await votes2024Data();
-    }
-    data = data;
-    console.log(data);
-  }
   
   onMount(async () => {
-    loadData();
+    data = await loadData(selected_year);
   })
 
   async function setYear(year) {
     if (year === selected_year) return;
     selected_year = year;
-    await loadData();
+    data = await loadData(selected_year);
   }
 
   async function setElection(election) {
     if (election === selected_election) return;
     selected_election = election;
-    await loadData();
+    data = await loadData(selected_year);
   }
 
   let innerWidth = 0;
@@ -69,10 +49,9 @@
     </div>
     <div class="select-button-wrapper">
       <div class="election-year">
-        <button class:selected={selected_year === 2009} on:click={() => setYear(2009)}> 2009</button>
-        <button class:selected={selected_year === 2014} on:click={() => setYear(2014)}> 2014</button>
-        <button class:selected={selected_year === 2019} on:click={() => setYear(2019)}> 2019 </button>
-        <button class:selected={selected_year === 2024} on:click={() => setYear(2024)}> 2024</button>
+        {#each years as year}
+          <button class:selected={selected_year === year} on:click={() => setYear(year)}> {year}</button>
+        {/each}
       </div>
     </div>
   </div>

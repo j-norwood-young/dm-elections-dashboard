245262040492f54ecf54dcdfbf29678a02a4bc99
Segundev <jayesegun03@gmail.com>
2024-05-12 14:38:33 +0100

    changed specified numbers of PR seats

--- packages/election-engine-map/src/lib/libs/seats.js
+++ packages/election-engine-map/src/lib/libs/seats.js
@@ -1,11 +1,11 @@
 export const regionalSeatAllocation = [
 	{ region: 'Eastern Cape', seat: 25 },
-	{ region: 'Free State', seat: 10 },
-	{ region: 'Gauteng', seat: 47 },
+	{ region: 'Free State', seat: 11 },
+	{ region: 'Gauteng', seat: 48 },
 	{ region: 'KwaZulu-Natal', seat: 41 },
-	{ region: 'Limpopo', seat: 20 },
+	{ region: 'Limpopo', seat: 19 },
 	{ region: 'Mpumalanga', seat: 15 },
 	{ region: 'North West', seat: 13 },
 	{ region: 'Northern Cape', seat: 5 },
-	{ region: 'Western Cape', seat: 24 }
+	{ region: 'Western Cape', seat: 23 }
 ];

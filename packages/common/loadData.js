15a18c97c5e18f5064630bc4c824f316b30a6c48
Jason Norwood-Young <jason@10layer.com>
2024-05-14 09:04:27 +0200

    Perfection is a process... Takes year and type, still to implement election and region

--- packages/common/loadData.js
+++ packages/common/loadData.js
@@ -8,8 +8,31 @@ const seatsUrl = "/results/seats/national";
  * @param {number} year - The year for which to load the data.
  * @returns {Promise<any>} - A promise that resolves to the loaded data.
  */
-export async function loadData({year = 2024, election = "National Assembly", region = "National"}) {
-    const url = `${baseUrl}/data/${election}/${region}/${year}`;
-    const res = await fetch(url);
-    return await res.json();
+export async function loadData({year = 2024, election = "National Assembly", region = "National", type="votes"}) {
+    // const url = `${baseUrl}/data/${election}/${region}/${year}`;
+    if (type === "votes") {
+        const url = `${baseUrl}${votesUrl}/${year}`;
+        return await load(url);
+    } else if (type === "seats") {
+        const url = `${baseUrl}${seatsUrl}/${year}`;
+        return await load(url);
+    }
+}
+
+/**
+ * Loads data from the specified URL.
+ * @param {string} url - The URL to fetch the data from.
+ * @returns {Promise<any>} - A promise that resolves to the parsed JSON data.
+ * @throws {Error} - If the data fails to load or parse.
+ */
+async function load(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Failed to load data from ${url}`);
+    }
+    try {
+        return await response.json();
+    } catch (error) {
+        throw new Error(`Failed to parse data from ${url}`);
+    }
 }

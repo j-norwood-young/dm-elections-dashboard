ebb403ba026a4b5db63e4df860e3890aae918992
Jason Norwood-Young <jason@10layer.com>
2024-05-14 12:41:40 +0200

    Table looking good

--- packages/election-engine-table/src/App.svelte
+++ packages/election-engine-table/src/App.svelte
@@ -1,7 +1,9 @@
 <script>
     import { onMount } from "svelte";
     import { loadData } from "@election-engine/common/loadData.js";
+    import { partyColor } from "@election-engine/common/colors.js";
     import YEARS from "@election-engine/common/years.json";
+    
     export let selected_year = 2019; // 2024, 2019, 2014
     export let selected_election = "National Assembly"; // National Assembly, Provincial Legislature
     export let selected_region = "National"; // National, Gauteng, Western Cape, etc.
@@ -33,6 +35,7 @@
         });
         
         const parties = {};
+        let total_votes = 0;
         for(let province_result of vote_results) {
             for (let party_result of province_result.PartyBallotResults) {
                 if (parties[party_result.Abbreviation]) {
@@ -45,8 +48,10 @@
                         seats: 0
                     };
                 }
+                total_votes += party_result.Votes;
             }
         }
+        let total_seats = 0;
         const seat_results = await loadData({
             year, 
             election: selected_election, 
@@ -64,9 +69,12 @@
                     seats: party_result.Seats
                 };
             }
+            total_seats += party_result.Seats;
         }
         const party_results = [];
         for (let party in parties) {
+            parties[party].votes_perc = Math.round(parties[party].votes / total_votes * 100);
+            parties[party].seats_perc = Math.round(parties[party].seats / total_seats * 100);
             party_results.push(parties[party]);
         }
         party_results.sort((a, b) => b.votes - a.votes);
@@ -75,14 +83,14 @@
 
     async function processData(year) {
         const current_year = await getData(year);
-        if (year > 2019) {
+        if (year > 2009) {
             const previous_year = await getData(year - 5);
             for (let party of current_year) {
                 const previous_party = previous_year.find(p => p.partyID === party.partyID);
                 if (previous_party) {
-                    party.change = party.seats - previous_party.seats;
+                    party.change = party.votes_perc - previous_party.votes_perc;
                 } else {
-                    party.change = party.seats;
+                    party.change = null;
                 }
             }
         }
@@ -105,19 +113,30 @@
     <div class="electionengine-table-container">
         <table class="electionengine-table">
             <thead>
-                <tr>
+                <tr style:border-left="6px #ccc solid">
                     <th class="electionengine-party-column">Party</th>
                     <th class="electionengine-seats-column">Seats</th>
                     <th class="electionengine-votes-column">Votes</th>
+                    <th class="electionengine-change-column">Change</th>
                 </tr>
             </thead>
             <tbody>
                 {#if data}
-                    {#each data as row}
-                        <tr>
+                    {#each data as row, i}
+                        <tr style:border-left="6px {partyColor(row.partyID, i)} solid">
                             <td class="electionengine-party-column">{row.name}</td>
                             <td class="electionengine-seats-column">{row.seats}</td>
                             <td class="electionengine-votes-column">{Intl.NumberFormat("en-US").format(row.votes)}</td>
+                            <td class="electionengine-change-column">
+                                {#if row.change > 0}
+                                    <div class="electionengine-label electionengine-change-up">+{row.change}%</div>
+                                {:else if row.change === 0}
+                                    <div class="electionengine-label electionengine-change-nochange">0%</div>
+                                {:else if row.change < 0}
+                                    <div class="electionengine-label electionengine-change-down">{row.change}%</div>
+                                {:else}
+                                    <div class="electionengine-label electionengine-change-na">N/A</div>
+                                {/if}
                         </tr>
                     {/each}
                 {/if}
@@ -148,13 +167,17 @@
     }
 
     table.electionengine-table {
-        width: 500px;
+        width: 550px;
         border-collapse: collapse;
     }
 
     table.electionengine-table th, table.electionengine-table td {
         border: 1px solid #ddd;
         padding: 8px;
+    }
+
+    table.electionengine-table th {
+        text-align: center;
     }
 
     .electionengine-table thead {
@@ -170,8 +193,35 @@
         text-align: right;
     }
 
+    .electionengine-change-column {
+        width: 50px;
+        text-align: center;
+    }
+
     .electionengine-votes-column {
         width: 100px;
         text-align: right;
+    }
+
+    .electionengine-label {
+        padding: 5px;
+        border-radius: 5px;
+        color: white;
+        font-size: 10px;
+        text-align: center;
+        width: 40px;
+        background-color: #444;
+    }
+    
+    .electionengine-change-up {
+        background-color: #4caf50;
+    }
+
+    .electionengine-change-down {
+        background-color: #f44336;
+    }
+
+    .electionengine-change-nochange {
+        background-color: #7a7a7a;
     }
 </style>

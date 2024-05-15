e89fc32187720712d70a5c9f713362b677a1ba9c
Mina Demian <mina@minademian.com>
2024-05-15 18:55:42 +0200

    chore: remove tailwind

--- packages/election-engine-hemicycle/postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-import tailwindcss from "tailwindcss";
-import { tailwindConfig } from "./tailwind.config.mjs";
-import autoprefixer from "autoprefixer";
-const postCssConfig = {
-  plugins: [tailwindcss(tailwindConfig), autoprefixer],
-};
-
-export default postCssConfig;

6442bf312e1ef899f2d19ee5cf85e7c9c3addbc0
Jason Norwood-Young <jason@10layer.com>
2024-05-19 16:00:05 +0200

    Hemicycle working with new API endpoints

--- packages/election-engine-hemicycle/src/main.ts
+++ packages/election-engine-hemicycle/src/main.ts
@@ -4,6 +4,7 @@ import App from "./App.svelte";
 const app = new App({
     target: document.getElementById("app")!,
     props: {
+        show_buttons: true,
         selected_year: 2019,
         selected_election: "National Assembly",
         selected_region: "National",

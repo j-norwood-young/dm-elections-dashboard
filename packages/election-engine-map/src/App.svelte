2ecbe58bad793dd7266ac61d4fd28ed7e4874b63
Jason Norwood-Young <jason@10layer.com>
2024-05-24 15:05:33 +0200

    SVG map - Put hexagons in SVG - Fix popover positioning

--- packages/election-engine-map/src/App.svelte
+++ packages/election-engine-map/src/App.svelte
@@ -1,126 +1,131 @@
 <script>
-  // @ts-nocheck
+    // @ts-nocheck
 
-  import { loadData } from "@election-engine/common/loadData.js";
-  import { onMount } from "svelte";
+    import { loadData } from "@election-engine/common/loadData.js";
+    import { onMount } from "svelte";
 
-  import NationalView from "./lib/components/dashboard-view/nationalView.svelte";
-  import ProvincialView from "./lib/components/dashboard-view/provincialView.svelte";
-  import years from "@election-engine/common/years.json";
-  import PROVINCES from "@election-engine/common/provinces.json";
+    import NationalView from "./lib/components/dashboard-view/nationalView.svelte";
+    import ProvincialView from "./lib/components/dashboard-view/provincialView.svelte";
+    import years from "@election-engine/common/years.json";
+    import PROVINCES from "@election-engine/common/provinces.json";
 
-  // Parameters
-  export let selected_year = 2019; // 2024, 2019, 2014
-  export let selected_election = "National Assembly"; // National Assembly, Provincial Legislature
-  export let selected_region = "National"; // National, Gauteng, Western Cape, etc.
-  export let show_buttons = false;
+    // Parameters
+    export let selected_year = 2019; // 2024, 2019, 2014
+    export let selected_election = "National Assembly"; // National Assembly, Provincial Legislature
+    export let selected_region = "National"; // National, Gauteng, Western Cape, etc.
+    export let show_buttons = false;
 
-  let provinces = PROVINCES;
-  let loading = false;
+    let provinces = PROVINCES;
+    let loading = false;
 
-  let data;
+    let data;
 
-  onMount(async () => {
-    data = await getData(selected_year);
-    loading = true;
-  });
+    onMount(async () => {
+        data = await getData(selected_year);
+        loading = true;
+    });
 
-  async function setYear(year) {
-    if (year === selected_year) return;
-    selected_year = year;
-    data = await getData(selected_year);
-  }
-
-  async function setElection(election) {
-    if (election === selected_election) return;
-    selected_election = election;
-    if (election === "Provincial Legislature") {
-      provinces = PROVINCES.filter((p) => !["National", "Out of Country"].includes(p));
-    } else {
-      provinces = PROVINCES;
-    }
-    data = await getData(selected_year, selected_election, selected_region);
-  }
-
-  async function setRegion(region) {
-    if (region === selected_region) return;
-    selected_region = region;
-    data = await getData(selected_year, selected_election, selected_region);
-  }
-
-  async function getData(year) {
-    // console.log(year, selected_election, selected_region);
-
-    if (selected_election === "National Assembly") {
-      const national_seats_result = await loadData({
-        year,
-        selected_region,
-      });
-
-      return national_seats_result.provincial_results;
+    async function setYear(year) {
+        if (year === selected_year) return;
+        selected_year = year;
+        data = await getData(selected_year);
     }
 
-    if (selected_region !== "National") {
-      const provincial_seats_result = await loadData({
-        year,
-        election: selected_election,
-        region: selected_region,
-      });
-
-      return provincial_seats_result.municipal_results;
+    async function setElection(election) {
+        if (election === selected_election) return;
+        selected_election = election;
+        if (election === "Provincial Legislature") {
+            provinces = PROVINCES.filter(
+                (p) => !["National", "Out of Country"].includes(p)
+            );
+        } else {
+            provinces = PROVINCES;
+        }
+        data = await getData(selected_year, selected_election, selected_region);
     }
-  }
 
-  // window width definition
-  let innerWidth = 0;
+    async function getData() {
+        console.log(selected_year, selected_election, selected_region);
+
+        if (selected_election === "National Assembly") {
+            const national_seats_result = await loadData({
+                year: selected_year,
+                region: selected_region,
+            });
+            return national_seats_result.provincial_results;
+        }
+
+        if (selected_region !== "National") {
+            const provincial_seats_result = await loadData({
+                year: selected_year,
+                election: selected_election,
+                region: selected_region,
+            });
+
+            return provincial_seats_result.municipal_results;
+        }
+    }
+
+    // window width definition
+    let innerWidth = 0;
 </script>
 
 <svelte:window bind:innerWidth />
 
 <div class="electionengine-mapsection">
-  {#if show_buttons}
-    <div class="electionengine-selectbutton-wrapper">
-      <button
-        class:selected={selected_election === "National Assembly"}
-        on:click={() => setElection("National Assembly")}
-      >
-        National Assembly</button
-      >
-      <button
-        class:selected={selected_election === "Provincial Legislature"}
-        on:click={() => setElection("Provincial Legislature")}
-      >
-        Provincial Legislature
-      </button>
-    </div>
+    {#if show_buttons}
+        <div class="electionengine-selectbutton-wrapper">
+            <button
+                class:selected={selected_election === "National Assembly"}
+                on:click={() => setElection("National Assembly")}
+            >
+                National Assembly</button
+            >
+            <button
+                class:selected={selected_election === "Provincial Legislature"}
+                on:click={() => setElection("Provincial Legislature")}
+            >
+                Provincial Legislature
+            </button>
+        </div>
 
-    <div class="electionengine-selectbutton-wrapper">
-      {#each years as year}
-        <button class:selected={selected_year === year} on:click={() => setYear(year)}> {year}</button>
-      {/each}
-    </div>
-  {/if}
-
-  {#if loading}
-    {#if selected_election === "National Assembly"}
-      {#if data}
-        <NationalView {data} {innerWidth} />
-      {/if}
-    {:else if selected_election === "Provincial Legislature"}
-      <ProvincialView {selected_year} bind:selected_region bind:provinces {data} />
+        <div class="electionengine-selectbutton-wrapper">
+            {#each years as year}
+                <button
+                    class:selected={selected_year === year}
+                    on:click={() => setYear(year)}
+                >
+                    {year}</button
+                >
+            {/each}
+        </div>
     {/if}
-  {:else}
-    <div>...Loading</div>
-  {/if}
+
+    {#if loading}
+        {#if selected_election === "National Assembly"}
+            {#if data}
+                <NationalView bind:data bind:innerWidth bind:selected_year />
+            {/if}
+        {:else if selected_election === "Provincial Legislature"}
+            <ProvincialView
+                {selected_year}
+                bind:selected_region
+                bind:provinces
+                {data}
+            />
+        {/if}
+    {:else}
+        <div>...Loading</div>
+    {/if}
 </div>
 
 <style>
-  .electionengine-selectbutton-wrapper {
-    margin-bottom: 0.5rem;
-  }
+    .electionengine-selectbutton-wrapper {
+        margin-bottom: 0.5rem;
+    }
 
-  button.selected {
-    background-color: #4caf50;
-    color: white;
-  }
+    button.selected {
+        background-color: #4caf50;
+        color: white;
+    }
 </style>
--- packages/election-engine-map/src/lib/components/tooltip.svelte
+++ packages/election-engine-map/src/lib/components/tooltip.svelte
@@ -1,120 +1,139 @@
 <script>
-  import { fly, fade } from "svelte/transition";
-  import { numberWithCommas } from "../libs/utils";
-  export let tooltipData;
-  export let grid;
-  // export let width;
+    // @ts-nocheck
+    import { fly, fade } from "svelte/transition";
+    import { numberWithCommas } from "../libs/utils";
+    export let tooltipData;
+    // export let width;
 
-  let tooltipWidth;
-  let tooltipHeight;
-  let width;
-  let height;
+    let tooltipWidth = 200;
+    let tooltipHeight = 200;
+    let width;
+    let height;
 
-  const xNudge = grid ? 5 : 65;
-  const yNudge = grid ? 45 : 95;
+    const xNudge = 30;
+    const yNudge = 15;
 
-  // If the x position + the tooltip width exceeds the chart width, offset backward to prevent overflow
-  $: xPosition =
-    tooltipData.x + tooltipWidth + xNudge > width ? tooltipData.x - tooltipWidth - xNudge : tooltipData.x + xNudge;
-  $: yPosition =
-    tooltipData.y + tooltipHeight + yNudge > height ? tooltipData.y - tooltipHeight - yNudge : tooltipData.y - yNudge;
+    // If the x position + the tooltip width exceeds the chart width, offset backward to prevent overflow
+    $: xPosition =
+        tooltipData.x + tooltipWidth + xNudge > width
+            ? tooltipData.x - tooltipWidth - xNudge
+            : tooltipData.x + xNudge;
+    $: yPosition =
+        tooltipData.y + tooltipHeight + yNudge > height
+            ? tooltipData.y - tooltipHeight - yNudge
+            : tooltipData.y - yNudge;
 </script>
 
 <svelte:window bind:innerWidth={width} bind:innerHeight={height} />
 
 <div
-  in:fly={{ y: 10, duration: 200, delay: 200 }}
-  out:fade
-  bind:clientWidth={tooltipWidth}
-  bind:clientHeight={tooltipHeight}
-  class="electionengine-tooltip-wrapper"
-  style="border-left-color:{tooltipData.color}; top:{yPosition}px; left:{xPosition}px"
+    in:fly={{ y: 10, duration: 200, delay: 200 }}
+    out:fade
+    bind:clientWidth={tooltipWidth}
+    bind:clientHeight={tooltipHeight}
+    class="electionengine-tooltip-wrapper"
+    style="border-left-color:{tooltipData.color}; top:{yPosition}px; left:{xPosition}px; position:fixed"
 >
-  <div class="electionengine-tooltip-container">
-    <div class="electionengine-tooltip-section">
-      <div class="electionengine-tooltip-thead">Party:</div>
-      <div class="electionengine-tooltip-tdata">{tooltipData.party.party_name}</div>
-    </div>
-    <div class="electionengine-tooltip-section">
-      <div class="electionengine-tooltip-thead">Percentage of Seats Won in Limpopo</div>
-      <div class="electionengine-tooltip-range-wrapper electionengine-tooltip-tdata">
-        <div class="electionengine-tooltip-range">
-          <div class="electionengine-tooltip-outer">
-            <div
-              class="electionengine-tooltip-inner"
-              style="width:{tooltipData.party.vote_perc}%; background:{tooltipData.color}"
-            ></div>
-          </div>
+    <div class="electionengine-tooltip-container">
+        <div class="electionengine-tooltip-section">
+            <div class="electionengine-tooltip-thead">Party:</div>
+            <div class="electionengine-tooltip-tdata">
+                {tooltipData.party.party_name}
+            </div>
         </div>
-        <span> {Math.round(tooltipData.party.vote_perc)}%</span>
-      </div>
+        <div class="electionengine-tooltip-section">
+            <div class="electionengine-tooltip-thead">
+                Percentage of Seats Won in Limpopo
+            </div>
+            <div
+                class="electionengine-tooltip-range-wrapper electionengine-tooltip-tdata"
+            >
+                <div class="electionengine-tooltip-range">
+                    <div class="electionengine-tooltip-outer">
+                        <div
+                            class="electionengine-tooltip-inner"
+                            style="width:{tooltipData.party
+                                .vote_perc}%; background:{tooltipData.color}"
+                        ></div>
+                    </div>
+                </div>
+                <span> {Math.round(tooltipData.party.vote_perc)}%</span>
+            </div>
+        </div>
+        <div class="electionengine-tooltip-section">
+            <div class="electionengine-tooltip-thead">
+                Total Number of Seats Won in Limpopo
+            </div>
+            <div class="electionengine-tooltip-tdata">
+                {tooltipData.party.seats} / {tooltipData.total_seats}
+            </div>
+        </div>
+        <div class="electionengine-tooltip-section">
+            <div class="electionengine-tooltip-thead">Total Votes</div>
+            <div class="electionengine-tooltip-tdata">
+                {numberWithCommas(tooltipData.party.votes)}
+            </div>
+        </div>
     </div>
-    <div class="electionengine-tooltip-section">
-      <div class="electionengine-tooltip-thead">Total Number of Seats Won in Limpopo</div>
-      <div class="electionengine-tooltip-tdata">{tooltipData.party.seats} / {tooltipData.total_seats}</div>
-    </div>
-    <div class="electionengine-tooltip-section">
-      <div class="electionengine-tooltip-thead">Total Votes</div>
-      <div class="electionengine-tooltip-tdata">{numberWithCommas(tooltipData.party.votes)}</div>
-    </div>
-  </div>
 </div>
 
 <style>
-  .electionengine-tooltip-wrapper {
-    width: 200px;
-    position: absolute;
-    background: #fffff9;
-    padding: 0.55rem;
-    border: 1px solid #c7c4c4;
-    border-left-width: 6px;
-    border-left-style: solid;
-  }
+    .electionengine-tooltip-wrapper {
+        width: 200px;
+        position: absolute;
+        background: #fffff9;
+        padding: 0.55rem;
+        border: 1px solid #c7c4c4;
+        border-left-width: 6px;
+        border-left-style: solid;
+    }
 
-  .electionengine-tooltip-section {
-    padding-bottom: 4px;
-  }
-  .electionengine-tooltip-thead {
-    font-size: 11px;
-    color: #999494;
-  }
+    .electionengine-tooltip-section {
+        padding-bottom: 4px;
+    }
+    .electionengine-tooltip-thead {
+        font-size: 11px;
+        color: #999494;
+    }
 
-  .electionengine-tooltip-tdata {
-    font-size: 13px;
-    color: #2a2a2a;
-    font-weight: bold;
-    border-bottom: 1px solid #c7c4c4;
-  }
+    .electionengine-tooltip-tdata {
+        font-size: 13px;
+        color: #2a2a2a;
+        font-weight: bold;
+        border-bottom: 1px solid #c7c4c4;
+    }
 
-  .electionengine-tooltip-range-wrapper {
-    display: flex;
-    gap: 12px;
-    justify-content: space-between;
-    align-items: center;
-  }
+    .electionengine-tooltip-range-wrapper {
+        display: flex;
+        gap: 12px;
+        justify-content: space-between;
+        align-items: center;
+    }
 
-  .electionengine-tooltip-range {
-    position: relative;
-    width: 100%;
-    height: 12px;
-  }
+    .electionengine-tooltip-range {
+        position: relative;
+        width: 100%;
+        height: 12px;
+    }
 
-  .electionengine-tooltip-outer {
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    background: #f2f2f2;
-    border-radius: 12px;
-  }
+    .electionengine-tooltip-outer {
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        background: #f2f2f2;
+        border-radius: 12px;
+    }
 
-  .electionengine-tooltip-inner {
-    height: 100%;
-    width: 73%;
-    position: absolute;
-    border-radius: inherit;
-  }
+    .electionengine-tooltip-inner {
+        height: 100%;
+        width: 73%;
+        position: absolute;
+        border-radius: inherit;
+    }
 
-  .electionengine-tooltip-container > div:last-child .electionengine-tooltip-tdata {
-    border-bottom: none;
-  }
+    .electionengine-tooltip-container
+        > div:last-child
+        .electionengine-tooltip-tdata {
+        border-bottom: none;
+    }
 </style>

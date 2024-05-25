a565e1249dbedc855c9ee1772bec92a920f31be8
Jason Norwood-Young <jason@10layer.com>
2024-05-25 10:36:54 +0200

    Provincial Map Refactor - Load correct region - Refactor code - Better key display

--- packages/election-engine-map/src/lib/components/provincialLegend.svelte
+++ packages/election-engine-map/src/lib/components/provincialLegend.svelte
@@ -1,83 +1,75 @@
 <script>
-  import SelectButton from "./selectButton.svelte";
-  import { partyColor } from "@election-engine/common/colors";
+    // @ts-nocheck
+    import { partyColor } from "@election-engine/common/colors";
 
-  export let highParty;
+    export let highParty;
 
-  let sortedPartySeats = [];
+    let sortedPartySeats = [];
 
-  const getUniqueParties = (data) => {
-    const seenAbbreviations = new Set();
+    const getUniqueParties = (data) => {
+        const seenAbbreviations = new Set();
 
-    return data.filter((item) => {
-      if (seenAbbreviations.has(item.party_abbreviation)) {
-        return false;
-      } else {
-        seenAbbreviations.add(item.party_abbreviation);
-        return true;
-      }
-    });
-  };
+        return data.filter((item) => {
+            if (seenAbbreviations.has(item.party_abbreviation)) {
+                return false;
+            } else {
+                seenAbbreviations.add(item.party_abbreviation);
+                return true;
+            }
+        });
+    };
 
-  $: sortedPartySeats = getUniqueParties(highParty);
+    $: sortedPartySeats = getUniqueParties(highParty);
 </script>
 
-<div class="electionengine-legend-heading">Party Colors Legend</div>
-<SelectButton>
-  <div class="legend-wrapper">
-    <div class="legend">
-      {#each sortedPartySeats as seat, index}
+<div class="legend">
+    {#each sortedPartySeats as seat, index}
         <div class="party-wrapper">
-          <svg width="12px" height="13px">
-            <g
-              transform="translate(1, 1)"
-              fill={partyColor(seat.party_abbreviation, index)}
-              fill-rule="nonzero"
-              stroke="#444444"
-            >
-              <polygon points="5,0 10,2.75 10,8.25 5,11 0,8.25 0,2.75"></polygon>
-            </g>
-          </svg>
-          <div class="electionengine-legend-partyname">{seat.party_abbreviation} [{seat.seats}]</div>
+            <svg width="12px" height="13px">
+                <g
+                    transform="translate(1, 1)"
+                    fill={partyColor(seat.party_abbreviation, index)}
+                    fill-rule="nonzero"
+                    stroke="#444444"
+                >
+                    <polygon points="5,0 10,2.75 10,8.25 5,11 0,8.25 0,2.75"
+                    ></polygon>
+                </g>
+            </svg>
+            <div class="electionengine-legend-partyname">
+                {seat.party_abbreviation} ({seat.seats} seats)
+            </div>
         </div>
-      {/each}
-    </div>
-  </div>
-</SelectButton>
+    {/each}
+</div>
 
 <style>
-  .legend-wrapper {
-    position: relative;
-    z-index: 99;
-  }
+    .legend {
+        background-color: #ffffff;
+        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+        position: absolute;
+        z-index: 99;
+        display: flex;
+        gap: 5px;
+        padding: 5px;
+        margin: 10px 0px;
+        border-radius: 4px;
+        justify-content: flex-start;
+        flex-direction: column;
+    }
 
-  .legend {
-    display: flex;
-    gap: 1rem;
-    padding: 10px 10px;
-    border-radius: 4px;
-    justify-content: flex-start;
-  }
+    .party-wrapper {
+        display: flex;
+        flex-direction: row;
+        gap: 5px;
+        justify-content: left;
+        align-items: center;
+        flex-shrink: 0;
+    }
 
-  .party-wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex-shrink: 0;
-  }
-
-  .electionengine-legend-heading {
-    font-size: 12px;
-    font-weight: 600;
-    color: #232323;
-    margin: 0;
-    padding-bottom: 2px;
-  }
-
-  .electionengine-legend-partyname {
-    color: #292929;
-    font-size: 10px;
-    font-weight: 600;
-  }
+    .electionengine-legend-partyname {
+        color: #292929;
+        font-size: 10px;
+        font-weight: 600;
+    }
 </style>

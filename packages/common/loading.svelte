7c787bc2a63d9407fb9557f346f66db0cedb3e87
Jason Norwood-Young <jason@10layer.com>
2024-05-25 12:23:07 +0200

    Loading widget

---
+++ packages/common/loading.svelte
@@ -0,0 +1,92 @@
+<script>
+    import { fade } from "svelte/transition";
+
+    let fadeConfig = {
+        duration: 300,
+    };
+
+    $: {
+        if (loading) {
+            fadeConfig = {
+                duration: 300,
+                delay: 0,
+            };
+        } else {
+            fadeConfig = {
+                duration: 300,
+                delay: 300,
+            };
+        }
+    }
+    export let loading = false;
+</script>
+
+{#if loading}
+    <div
+        class="loading"
+        style="display: {loading ? 'flex' : 'none'};"
+        transition:fade={fadeConfig}
+    >
+        <div class="loader"></div>
+    </div>
+{/if}
+
+<style>
+    .loading {
+        display: flex;
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        background-color: rgba(255, 255, 255, 0.8);
+        z-index: 9999;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .loader {
+        opacity: 0.8;
+        width: 45px;
+        aspect-ratio: 1;
+        --c: no-repeat linear-gradient(#000 0 0);
+        background:
+            var(--c) 0% 50%,
+            var(--c) 50% 50%,
+            var(--c) 100% 50%;
+        background-size: 20% 100%;
+        animation: l1 2s infinite linear;
+    }
+    @keyframes l1 {
+        0% {
+            background-size:
+                20% 100%,
+                20% 100%,
+                20% 100%;
+        }
+        33% {
+            background-size:
+                20% 10%,
+                20% 100%,
+                20% 100%;
+        }
+        50% {
+            background-size:
+                20% 100%,
+                20% 10%,
+                20% 100%;
+        }
+        66% {
+            background-size:
+                20% 100%,
+                20% 100%,
+                20% 10%;
+        }
+        100% {
+            background-size:
+                20% 100%,
+                20% 100%,
+                20% 100%;
+        }
+    }
+</style>

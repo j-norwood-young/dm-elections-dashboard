860c76a9e45d4dc610bdbfb8e800baa3825806df
Jason Norwood-Young <jason@10layer.com>
2024-06-05 14:21:24 +0200

    v0.9.1 - Bug fix

--- election-engine.php
+++ election-engine.php
@@ -5,7 +5,7 @@
  * Description:       Visualisations for the South African general elections.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.9.0
+ * Version:           0.9.1
  * Author:            10Layer
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-define("ELECTIONENGINE_VERSION", '0.9.0');
+define("ELECTIONENGINE_VERSION", '0.9.1');
 define("ELECTIONENGINE_DIST_DIR", __DIR__ . '/packages/election-engine-wordpress-block/dist/');
 define("ELECTIONENGINE_DIST_URL", plugins_url('packages/election-engine-wordpress-block/dist/', __FILE__));
 define("ELECTIONENGINE_BLOCK_NAME", 'tenlayer/election-engine');
@@ -94,7 +94,7 @@ function electionengine_shortcode($atts)
 		'selected_election' => 'National Assembly',
 		'selected_year' => '2019',
 		'selected_region' => 'National',
-		'selected_ballot' => ['Combined'],
+		'selected_ballot' => 'Combined',
 		'show_title' => 'true',
 		'show_blurb' => 'true',
 		'show_buttons' => 'false',
--- packages/election-engine-wordpress-block/php/views/election-engine-shortcode-view.php
+++ packages/election-engine-wordpress-block/php/views/election-engine-shortcode-view.php
@@ -8,37 +8,42 @@ class ElectionEngineShortcodeView
 
 	function generate()
 	{
-		$visualisation = $this->atts['visualisation'] ?: "hemicycle";
-		$selected_year = $this->atts['selected_year'] ?: "2019";
-		$selected_election = $this->atts['selected_election'] ?: "National Assembly";
-		$selected_region = $this->atts['selected_region'] ?: "National";
-		$selected_ballot = $this->atts['selected_ballot'] ?: "Combined";
-		$show_title = $this->atts['show_title'] ? "1" : "0";
-		$show_blurb = $this->atts['show_blurb'] ? "1" : "0";
-		$show_buttons = $this->atts['show_buttons'] ? "1" : "0";
-		$embed = get_site_url() . '/election-engine/embed/' .
-			"?visualisation=" . urlencode($visualisation) .
-			"&selected_year=" . urlencode($selected_year) .
-			"&selected_election=" . urlencode($selected_election) .
-			"&selected_region=" . urlencode($selected_region) .
-			"&selected_ballot=" . urlencode($selected_ballot) .
-			"&show_title=" . urlencode($show_title) .
-			"&show_blurb=" . urlencode($show_blurb) .
-			"&show_buttons=" . urlencode($show_buttons);
-		$html = "<div
-				class=\"wp-block-tenlayer-election-engine\"
-				data-visualisation=\"{$visualisation}\"
-				data-selected_year=\"{$selected_year}\"
-				data-selected_election=\"{$selected_election}\"
-				data-selected_region=\"{$selected_region}\"
-				data-show_title=\"{$show_title}\"
-				data-show_blurb=\"{$show_blurb}\"
-				data-show_buttons=\"{$show_buttons}\"
-			>
-				<div class=\"election-engine-inline\" data-inline=\"$embed\"></div>
-			</div>";
-		wp_enqueue_script("electionengine-frontend-script", plugin_dir_url(__FILE__) . "../../dist/app/election-engine.umd.js", [], ELECTIONENGINE_VERSION, true);
-		wp_enqueue_style("electionengine-frontend-style", plugin_dir_url(__FILE__) . "../../dist/app/style.css", [], ELECTIONENGINE_VERSION);
-		return $html;
+		try {
+			$visualisation = $this->atts['visualisation'] ?: "hemicycle";
+			$selected_year = $this->atts['selected_year'] ?: "2019";
+			$selected_election = $this->atts['selected_election'] ?: "National Assembly";
+			$selected_region = $this->atts['selected_region'] ?: "National";
+			$selected_ballot = $this->atts['selected_ballot'] ?: "Combined";
+			$show_title = $this->atts['show_title'] ? "1" : "0";
+			$show_blurb = $this->atts['show_blurb'] ? "1" : "0";
+			$show_buttons = $this->atts['show_buttons'] ? "1" : "0";
+			$embed = get_site_url() . '/election-engine/embed/' .
+				"?visualisation=" . urlencode($visualisation) .
+				"&selected_year=" . urlencode($selected_year) .
+				"&selected_election=" . urlencode($selected_election) .
+				"&selected_region=" . urlencode($selected_region) .
+				"&selected_ballot=" . urlencode($selected_ballot) .
+				"&show_title=" . urlencode($show_title) .
+				"&show_blurb=" . urlencode($show_blurb) .
+				"&show_buttons=" . urlencode($show_buttons);
+			$html = "<div
+					class=\"wp-block-tenlayer-election-engine\"
+					data-visualisation=\"{$visualisation}\"
+					data-selected_year=\"{$selected_year}\"
+					data-selected_election=\"{$selected_election}\"
+					data-selected_region=\"{$selected_region}\"
+					data-selected_ballot=\"{$selected_ballot}\"
+					data-show_title=\"{$show_title}\"
+					data-show_blurb=\"{$show_blurb}\"
+					data-show_buttons=\"{$show_buttons}\"
+				>
+					<div class=\"election-engine-inline\" data-inline=\"$embed\"></div>
+				</div>";
+			wp_enqueue_script("electionengine-frontend-script", plugin_dir_url(__FILE__) . "../../dist/app/election-engine.umd.js", [], ELECTIONENGINE_VERSION, true);
+			wp_enqueue_style("electionengine-frontend-style", plugin_dir_url(__FILE__) . "../../dist/app/style.css", [], ELECTIONENGINE_VERSION);
+			return $html;
+		} catch (Exception $e) {
+			return ""; // Do nothing
+		}
 	}
 }
